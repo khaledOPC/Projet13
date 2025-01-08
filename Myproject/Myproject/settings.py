11ff7e0481a140fdc9e26eb76912ad01a375d680
Khaled.djema <khaled.djema92@gmail.com>
2025-01-08 01:02:27 +0100

    Updated urls.py and settings.py for improved routing and configuration

--- Myproject/Myproject/settings.py
+++ Myproject/Myproject/settings.py
@@ -37,8 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp'
+    'webapp',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        #'PAGE_SIZE': 10,
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Myproject/Myproject/urls.py
+++ Myproject/Myproject/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import requests
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
+from webapp.views import ActivityAPIView
+from webapp.views import UserRegistrationView, UserLoginView
+
+def home(request):
+    return HttpResponse("Page d'acceuil")
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
